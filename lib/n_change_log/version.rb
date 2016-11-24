e4eeccacd6765841fa337979c1a091b3c7ec554a
Naoto Kaneko <naoty.k@gmail.com>
2016-11-24 18:39:18 +0900

    Add Version#summary to output all changes in the version

--- lib/n_change_log/version.rb
+++ lib/n_change_log/version.rb
@@ -10,5 +10,22 @@ module NChangeLog
     def to_s
       (["## #{number}"] + issues.map(&:to_s)).join("\n\n")
     end
+
+    def summary
+      change_groups = {}
+
+      issues.each do |issue|
+        issue.change_groups.each do |change_group|
+          change_groups[change_group.name] ||= []
+          change_groups[change_group.name] += change_group.changes
+        end
+      end
+
+      change_group_texts = change_groups.map do |change_group_name, changes|
+        (["#### #{change_group_name}"] + changes.map(&:to_s)).join("\n")
+      end
+
+      (["## #{number}"] + change_group_texts).join("\n\n")
+    end
   end
 end
